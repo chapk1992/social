a202bd5ff63742c16c4a47ba46f99505a70938d9
Your Name <you@example.com>
2020-03-12 15:07:11 +0200

    MAP method

--- src/components/Container/Dialogs/Dialogs.jsx
+++ src/components/Container/Dialogs/Dialogs.jsx
@@ -1,8 +1,48 @@
 import React from "react";
 import con from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
-const Dialogs = () => {
-  return <div className={con.dialogs}>Dialogs</div>;
+const Dialog = props => {
+  return (
+    <div className={con.dialog}>
+      <NavLink to={"/dialogs/" + props.id} activeClassName={con.active}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+const Message = props => {
+  return <div className={con.message}>{props.message}</div>;
+};
+
+const Dialogs = props => {
+  let dialogs = [
+    { name: "Yaroslav", id: "1" },
+    { name: "Misha", id: "2" },
+    { name: "Roman", id: "3" },
+    { name: "Lera", id: "4" },
+    { name: "Anastasia", id: "5" }
+  ];
+
+  let messages = [
+    { id: "1", message: "Hi.How are you?" },
+    { id: "2", message: "I am fine.Thank you!" },
+    { id: "3", message: "I like React" }
+  ];
+
+  let dialogsElement = dialogs.map(d => {
+    return <Dialog name={d.name} id={d.id} />;
+  });
+
+  let messageElements = messages.map(m => {
+    return <Message message={m.message} id={m.id} />;
+  });
+  return (
+    <div className={con.dialogs}>
+      <div className={con.people}>{dialogsElement}</div>
+      <div className={con.messages}>{messageElements}</div>
+    </div>
+  );
 };
 
 export default Dialogs;
